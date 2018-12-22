9ceced6a987c918ea5cffb92b3e535f533eefca4
NegaAshley <ashleymxu@gmail.com>
2018-12-22 02:38:37 +0000

    Add index route and test blog model

--- app.js
+++ app.js
@@ -21,6 +21,17 @@ var blogSchema = new mongoose.Schema({
 //Compile into model
 var Blog = mongoose.model("Blog", blogSchema);
 
+Blog.create({
+    title: "Test Blog",
+    image: "https://scontent-ort2-1.xx.fbcdn.net/v/t1.0-9/20106290_10159031428835254_5819301612392045243_n.jpg?_nc_cat=110&_nc_ht=scontent-ort2-1.xx&oh=197585cbbcfb2c93bb53eb9c972903d8&oe=5CD6EA96",
+    body: "This is the body."
+});
+
+//Index route
+app.get("/blogs", function(req, res){
+   res.render("index"); 
+});
+
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Blog server has started!");
 });
