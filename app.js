44d6198ee391dfa754266e5899f54a14f3d97072
NegaAshley <ashleymxu@gmail.com>
2018-12-22 02:30:15 +0000

    Create blog schema

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
+var blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: {type: Date, default: Date.now}
+});
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Blog server has started!");
