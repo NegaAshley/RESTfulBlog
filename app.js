b81d4fe1da4f484c6ce8db951714f140a88c5697
NegaAshley <ashleymxu@gmail.com>
2018-12-22 02:31:55 +0000

    Compile into model

--- app.js
+++ app.js
@@ -5,16 +5,21 @@ var express     = require("express"),
     
 mongoose.connect("mongodb://localhost:27017/restful_blog_app", {useNewUrlParser: true });
 
+//App config
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
+//Mongoose/model config
 var blogSchema = new mongoose.Schema({
     title: String,
     image: String,
     body: String,
     created: {type: Date, default: Date.now}
 });
+
+//Compile into model
+var Blog = mongoose.model("Blog", blogSchema);
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Blog server has started!");
